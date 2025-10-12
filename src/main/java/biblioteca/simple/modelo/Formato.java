c401ce7cf27d7330bc485dd3321d1954ff39e7aa
rebeksaba <rebeksaba@gmail.com>
2025-10-12 20:46:55 +0200

    Creación del Proyecto GestionBiblioteca

---
+++ src/main/java/biblioteca/simple/modelo/Formato.java
@@ -0,0 +1,3 @@
+package biblioteca.simple.modelo;
+
+public enum Formato {FISICO, DIGITAL}
---
+++ src/main/java/biblioteca/simple/modelo/Producto.java
@@ -0,0 +1,9 @@
+package biblioteca.simple.modelo;
+
+public abstract class Producto {
+
+    protected int id;
+    protected String titulo;
+    protected String anio;
+    protected Formato formato;
+}
